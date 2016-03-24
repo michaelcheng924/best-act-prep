7a3e815f3a549e2eba79e430ce64a5662d2c7f54
michaelcheng429 <cheng@csdisco.com>
2016-03-23 21:08:20 -0500

    create MainHeader

--- app/components/Course/Main.jsx
+++ app/components/Course/Main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import MainHeader from 'components/Course/MainHeader';
 
 export default class CourseMain extends React.Component {
     render() {
         return (
             <div className="course-main">
+                <MainHeader />
                 MAIN
             </div>
         );
---
+++ app/components/Course/MainHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class MainHeader extends React.Component {
+    render() {
+        return (
+            <div className="main-header">
+                MAIN HEADER
+            </div>
+        );
+    }
+}
