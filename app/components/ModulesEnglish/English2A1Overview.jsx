266f80241136512ffe639d9cad1e1e710ec75d43
michaelcheng429 <cheng@csdisco.com>
2016-04-07 16:11:44 -0500

    video example

--- app/components/ModulesEnglish/English2A1Overview.jsx
+++ app/components/ModulesEnglish/English2A1Overview.jsx
@@ -7,6 +7,10 @@ export default class English2A1Overview extends React.Component {
                 <h4 className="module__title--category">2.A.1) ACT English Basics</h4>
                 <h2 className="module__title">Overview/Format</h2>
                 <div className="module__content">
+                    <video controls="controls">
+                        <source src="https://drive.google.com/uc?export=download&id=0B2BsXecFN2TxUWZ3bFFmeWUtMEU" type='video/mp4'/>
+                    </video>
+
                     <h3>Highlights</h3>
                 </div>
             </div>
