c12fb536551150b66f3dbdf2b33a87a4cd53ce18
michaelcheng429 <cheng@csdisco.com>
2016-04-22 16:44:12 -0500

    add vimeo math guided practice

--- app/components/ModulesOldCourse/OldCourse85MathGuidedPractice.jsx
+++ app/components/ModulesOldCourse/OldCourse85MathGuidedPractice.jsx
@@ -7,27 +7,13 @@ export default class OldCourse85MathGuidedPractice extends React.Component {
                 <h4 className="module__title--category">A.5) Old Course Videos</h4>
                 <h2 className="module__title">ACT Math Guided Practice</h2>
                 <div className="module__content">
-                    <video controls="controls" className="module__video-small" poster="/images/poster-2p-0-analyze.png">
-                        <source src="https://www.dropbox.com/s/cj2hw6yr9tsf98e/2p-0-analyze.mp4?raw=1" type="video/mp4"/>
-                    </video>
-                    <video controls="controls" className="module__video-small" poster="/images/poster-2p-1-first-30.png">
-                        <source src="https://www.dropbox.com/s/8px7lfbz81akhqg/2p-1-first-30.mp4?raw=1" type="video/mp4"/>
-                    </video>
-                    <video controls="controls" className="module__video-small" poster="/images/poster-2p-1-timer-first-30.png">
-                        <source src="https://www.dropbox.com/s/uoxvcldoej6gv1z/2p-2-timer-first-30.mp4?raw=1" type="video/mp4"/>
-                    </video>
-                    <video controls="controls" className="module__video-small" poster="/images/poster-2p-2-last-30.png">
-                        <source src="https://www.dropbox.com/s/l7ezc7nb86yxwf1/2p-3-last%2030.mp4?raw=1" type="video/mp4"/>
-                    </video>
-                    <video controls="controls" className="module__video-small" poster="/images/poster-2p-2-timer-last-30.png">
-                        <source src="https://www.dropbox.com/s/r32hs753t30dfx5/2p-4-timer-last-30.mp4?raw=1" type="video/mp4"/>
-                    </video>
-                    <video controls="controls" className="module__video-small" poster="/images/poster-2p-3-full-test.png">
-                        <source src="https://www.dropbox.com/s/wxqndikvl9wxoad/2p-5-full%20test.mp4?raw=1" type="video/mp4"/>
-                    </video>
-                    <video controls="controls" className="module__video-small" poster="/images/poster-2p-3-timer-full-test.png">
-                        <source src="https://www.dropbox.com/s/5xptkv7o5emlveg/2p-6-timer-full-test.mp4?raw=1" type="video/mp4"/>
-                    </video>
+                    <iframe src="https://player.vimeo.com/video/163876747" className="module__video-small" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                    <iframe src="https://player.vimeo.com/video/163876751" className="module__video-small" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                    <iframe src="https://player.vimeo.com/video/163876752" className="module__video-small" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                    <iframe src="https://player.vimeo.com/video/163876755" className="module__video-small" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                    <iframe src="https://player.vimeo.com/video/163876758" className="module__video-small" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                    <iframe src="https://player.vimeo.com/video/163876760" className="module__video-small" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                    <iframe src="https://player.vimeo.com/video/163876759" className="module__video-small" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
                 </div>
             </div>
         );
