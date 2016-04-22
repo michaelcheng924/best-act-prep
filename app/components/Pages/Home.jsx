958fe253be4dec2774057b02eee038135cacfd41
michaelcheng429 <cheng@csdisco.com>
2016-04-21 22:14:43 -0500

    add testimonials section

--- app/components/Pages/Home.jsx
+++ app/components/Pages/Home.jsx
@@ -25,16 +25,38 @@ export class Home extends React.Component {
     render() {
         return (
             <div className="home__page">
-                <h1 className="home__title">The Best ACT Prep Online Course</h1>
-                <div className="home__call-to-action">
-                    <img src="/images/michael.png" />
-                    <div className="home__call-to-action-text-container">
-                        <div className="home__call-to-action-text--odd">The most popular ACT instructor.</div>
-                        <div className="home__call-to-action-text--even">The best ACT strategies.</div>
-                        <div className="home__call-to-action-text--odd">Higher score guaranteed. No risk, no strings.</div>
+                <div className="home__call-to-action-container">
+                    <h1 className="home__title">The Best ACT Prep Online Course</h1>
+                    <div className="home__call-to-action">
+                        <img src="/images/michael.png" />
+                        <div className="home__call-to-action-text-container">
+                            <div className="home__call-to-action-text--odd">The most popular ACT instructor.</div>
+                            <div className="home__call-to-action-text--even">The best ACT strategies.</div>
+                            <div className="home__call-to-action-text--odd">Higher score guaranteed. No risk, no strings.</div>
+                        </div>
+                    </div>
+                    <a href="/why-best-act-prep"><button className="home__call-to-action-button btn">Learn More</button></a>
+                </div>
+
+                <div className="home__testimonials-container">
+                    <div className="home__testimonials-main">
+                        <span className="home__testimonials-main-icon glyphicon glyphicon-heart-empty" />
+                        <h2 className="home__testimonials-main-heading">Testimonials</h2>
+                        <div className="home__testimonials-main-tagline">
+                            See what students have said about us!
+                        </div>
+                    </div>
+                    <div className="home__testimonials-quotes-container">
+                        <div className="home__testimonials-quote-container">
+                            <div className="home__testimonials-quote">Coming soon :)</div>
+                            <div className="home__testimonials-author">- student</div>
+                        </div>
+                        <div className="home__testimonials-quote-container">
+                            <div className="home__testimonials-quote">Coming soon :)</div>
+                            <div className="home__testimonials-author">- student</div>
+                        </div>
                     </div>
                 </div>
-                <a href="/why-best-act-prep"><button className="home__call-to-action-button btn">Learn More</button></a>
             </div>
         );
     }
