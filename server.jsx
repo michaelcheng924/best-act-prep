88e877d73bdf0f9eb67cac07f4ebd122fdd67c21
michaelcheng429 <cheng@csdisco.com>
2016-04-25 23:30:49 -0500

    change analytics code

--- server.jsx
+++ server.jsx
@@ -95,7 +95,7 @@ app.use((req, res) => {
                       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
                       })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-                      ga('create', 'UA-76355515-1', 'auto');
+                      ga('create', 'UA-76974805-1', 'auto');
                       ga('send', 'pageview');
                     </script>
                 </head>
