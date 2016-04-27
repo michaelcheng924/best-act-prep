7d4b920ada85b5ef9f50a7ff0890f07dbcf66b02
michaelcheng429 <cheng@csdisco.com>
2016-04-27 14:40:33 -0500

    add to welcome

--- app/components/ModulesFoundations/Foundations10Welcome.jsx
+++ app/components/ModulesFoundations/Foundations10Welcome.jsx
@@ -7,7 +7,9 @@ export default class Foundations10Welcome extends React.Component {
                 <h4 className="module__title--category">1.0) ACT Foundations</h4>
                 <h2 className="module__title">Welcome and Instructions</h2>
                 <div className="module__content">
-                    CONTENT
+                    <p>Available videos so far:</p>
+                    <p><strong>Z) Old Course Videos - </strong> 65-70 videos (scroll down to the bottom of the sidebar)</p>
+                    <p><strong>Overview/Format Videos (1.1, 2.A.1, 3.A.1, 4.A.1, 5.A.1) -</strong> 5 videos (click on the specific links in the sidebar)</p>
                 </div>
             </div>
         );
