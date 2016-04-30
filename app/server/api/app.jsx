66722cffcff82444d3b9c86990596569f1480cbe
michaelcheng429 <cheng@csdisco.com>
2016-04-29 19:36:45 -0500

    JSON stringify object

--- app/server/api/app.jsx
+++ app/server/api/app.jsx
@@ -92,7 +92,7 @@ router.post('/buycourse', (req, res) => {
         from: 'COURSE BOUGHT <dev@bestactprep.co>',
         to: 'cheng.c.mike@gmail.com',
         subject: 'COURSE BOUGHT',
-        text: `Course bought - ${req.body}`
+        text: `Course bought - ${JSON.stringify(req.body)}`
     };
 
     mailgun.messages().send(MAILGUN_LOGGING_DATA, (error, body) => {
