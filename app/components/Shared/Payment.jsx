dcd09bc85b02682ad9760767981fb62ab775f8fd
michaelcheng429 <cheng@csdisco.com>
2016-05-07 13:50:05 -0500

    change 20 to 15

--- app/components/Shared/Payment.jsx
+++ app/components/Shared/Payment.jsx
@@ -74,7 +74,7 @@ export class Payment extends React.Component {
                             <div className="why__buy-price-current-text">Limited Time Beta Price!</div>
                         </div>
                     </div>
-                    <div className="why__buy-increase-price">Price will increase after the first 20 purchases!</div>
+                    <div className="why__buy-increase-price">Price will increase after the first <strike>20</strike> <strong>15</strong> purchases!</div>
                     <StripeCheckout
                         name="Best ACT Prep"
                         description="Online Course"
