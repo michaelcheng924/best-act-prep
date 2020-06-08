e4e1b3736b18fd478658ec6dddf57075436d48c1
Michael Cheng <mcheng1@wm.com>
2020-06-08 15:02:36 -0500

    fix science

--- react-ui/src/constants/videos-strategies.js
+++ react-ui/src/constants/videos-strategies.js
@@ -1,225 +1,225 @@
 const VIDEOS_GENERAL = [
-    {
-        title: 'Overview of the ACT',
-        id: '163876888'
-    },
-    {
-        title: 'The most important skill to develop to improve your ACT score',
-        id: '163876721',
-        restricted: true
-    },
-    {
-        title: 'Recommended reading list for the ACT',
-        id: '163876720',
-        restricted: true
-    },
-    {
-        title: 'What "optional" item do I NEED to bring to the ACT?',
-        id: '163876725',
-        restricted: true
-    }
+  {
+    title: "Overview of the ACT",
+    id: "163876888",
+  },
+  {
+    title: "The most important skill to develop to improve your ACT score",
+    id: "163876721",
+    restricted: true,
+  },
+  {
+    title: "Recommended reading list for the ACT",
+    id: "163876720",
+    restricted: true,
+  },
+  {
+    title: 'What "optional" item do I NEED to bring to the ACT?',
+    id: "163876725",
+    restricted: true,
+  },
 ];
 
 const VIDEOS_ENGLISH_STRATEGIES = [
-    {
-        title: 'Know the ACT English test BEFORE you take it',
-        id: '163876880'
-    },
-    {
-        title: 'How to manage time for the ACT English test',
-        id: '165519954'
-    },
-    {
-        title: 'Should I skip any questions on the ACT English test?',
-        id: '170987103'
-    },
-    {
-        title: 'How should I bubble for the ACT English test?',
-        id: '165700824'
-    },
-    {
-        title: 'Memorize grammar rules for the ACT English test'
-    },
-    {
-        title: 'Less is usually better',
-        id: '163876730'
-    },
-    {
-        title: 'Don\'t read "normally"',
-        id: '163876736',
-        restricted: true
-    },
-    {
-        title: 'Context is key',
-        id: '163876743',
-        restricted: true
-    },
-    {
-        title: 'Read "out loud"',
-        id: '163876748',
-        restricted: true
-    },
-    {
-        title: 'Common types of questions',
-        id: '163876745'
-    }
+  {
+    title: "Know the ACT English test BEFORE you take it",
+    id: "163876880",
+  },
+  {
+    title: "How to manage time for the ACT English test",
+    id: "165519954",
+  },
+  {
+    title: "Should I skip any questions on the ACT English test?",
+    id: "170987103",
+  },
+  {
+    title: "How should I bubble for the ACT English test?",
+    id: "165700824",
+  },
+  {
+    title: "Memorize grammar rules for the ACT English test",
+  },
+  {
+    title: "Less is usually better",
+    id: "163876730",
+  },
+  {
+    title: 'Don\'t read "normally"',
+    id: "163876736",
+    restricted: true,
+  },
+  {
+    title: "Context is key",
+    id: "163876743",
+    restricted: true,
+  },
+  {
+    title: 'Read "out loud"',
+    id: "163876748",
+    restricted: true,
+  },
+  {
+    title: "Common types of questions",
+    id: "163876745",
+  },
 ];
 
 const VIDEOS_MATH_STRATEGIES = [
-    {
-        title: 'Know the ACT math test BEFORE you take it',
-        id: '163876885'
-    },
-    {
-        title: 'How to manage time for the ACT Math test',
-        id: '165519995'
-    },
-    {
-        title: 'Should I skip any questions on the ACT math test?',
-        id: '170987105'
-    },
-    {
-        title: 'How should I bubble for the ACT math test?',
-        id: '165700825'
-    },
-    {
-        title: 'One minute per question is NOT a good timing strategy',
-        id: '163876761'
-    },
-    {
-        title: '"Cheat" using the answers',
-        id: '163876764'
-    },
-    {
-        title: '"Cheat" by plugging in easy numbers',
-        id: '163876777',
-        restricted: true
-    },
-    {
-        title: 'Take the ACT math test TWICE',
-        id: '163876779',
-        restricted: true
-    },
-    {
-        title: 'The truth about diagrams',
-        id: '163876782',
-        restricted: true
-    },
-    {
-        title: 'Using your calculator effectively',
-        id: '163876763',
-        restricted: true
-    }
+  {
+    title: "Know the ACT math test BEFORE you take it",
+    id: "163876885",
+  },
+  {
+    title: "How to manage time for the ACT Math test",
+    id: "165519995",
+  },
+  {
+    title: "Should I skip any questions on the ACT math test?",
+    id: "170987105",
+  },
+  {
+    title: "How should I bubble for the ACT math test?",
+    id: "165700825",
+  },
+  {
+    title: "One minute per question is NOT a good timing strategy",
+    id: "163876761",
+  },
+  {
+    title: '"Cheat" using the answers',
+    id: "163876764",
+  },
+  {
+    title: '"Cheat" by plugging in easy numbers',
+    id: "163876777",
+    restricted: true,
+  },
+  {
+    title: "Take the ACT math test TWICE",
+    id: "163876779",
+    restricted: true,
+  },
+  {
+    title: "The truth about diagrams",
+    id: "163876782",
+    restricted: true,
+  },
+  {
+    title: "Using your calculator effectively",
+    id: "163876763",
+    restricted: true,
+  },
 ];
 
 const VIDEOS_READING_STRATEGIES = [
-    {
-        title: 'PDF - Strategy summary',
-        id: 'reading-strategy',
-        pdf: '/review-guides/reading-strategy.pdf',
-        restricted: true,
-        buttonText: 'Open PDF',
-        isPdf: true
-    },
-    {
-        title: 'Know the ACT reading test BEFORE you take it',
-        id: '163876886'
-    },
-    {
-        title: 'How to manage time for the ACT Math test',
-        id: '165519952'
-    },
-    {
-        title: 'Should I skip any questions on the ACT reading test?',
-        id: '170987104'
-    },
-    {
-        title: 'How should I bubble for the ACT reading test?',
-        id: '165700827'
-    },
-    {
-        title: 'Should I read the passage or questions first?',
-        id: '163876802',
-        restricted: true
-    },
-    {
-        title: 'Should I do the prose passage last?',
-        id: '163876807',
-        restricted: true
-    },
-    {
-        title: 'How fast should I read each passage?',
-        id: '163876801',
-        restricted: true
-    },
-    {
-        title: 'What should I be looking for as I read?',
-        id: '163876799'
-    },
-    {
-        title: 'Why should I read with my finger?',
-        id: '163876808'
-    },
-    {
-        title: '4 types of questions to expect',
-        id: '163876809',
-        restricted: true
-    },
-    {
-        title: '5 common wrong answer choices',
-        id: '163876810',
-        restricted: true
-    },
-    {
-        title: 'Guess the answer first'
-    }
+  {
+    title: "PDF - Strategy summary",
+    id: "reading-strategy",
+    pdf: "/review-guides/reading-strategy.pdf",
+    restricted: true,
+    buttonText: "Open PDF",
+    isPdf: true,
+  },
+  {
+    title: "Know the ACT reading test BEFORE you take it",
+    id: "163876886",
+  },
+  {
+    title: "How to manage time for the ACT Math test",
+    id: "165519952",
+  },
+  {
+    title: "Should I skip any questions on the ACT reading test?",
+    id: "170987104",
+  },
+  {
+    title: "How should I bubble for the ACT reading test?",
+    id: "165700827",
+  },
+  {
+    title: "Should I read the passage or questions first?",
+    id: "163876802",
+    restricted: true,
+  },
+  {
+    title: "Should I do the prose passage last?",
+    id: "163876807",
+    restricted: true,
+  },
+  {
+    title: "How fast should I read each passage?",
+    id: "163876801",
+    restricted: true,
+  },
+  {
+    title: "What should I be looking for as I read?",
+    id: "163876799",
+  },
+  {
+    title: "Why should I read with my finger?",
+    id: "163876808",
+  },
+  {
+    title: "4 types of questions to expect",
+    id: "163876809",
+    restricted: true,
+  },
+  {
+    title: "5 common wrong answer choices",
+    id: "163876810",
+    restricted: true,
+  },
+  {
+    title: "Guess the answer first",
+  },
 ];
 
 const VIDEOS_SCIENCE_STRATEGIES = [
-    {
-        title: 'PDF - Strategy summary',
-        id: 'science-strategy',
-        pdf: '/review-guides/science-strategy.pdf',
-        restricted: true,
-        buttonText: 'Open PDF',
-        isPdf: true
-    },
-    {
-        title: 'Know the ACT reading test BEFORE you take it',
-        id: '163876886'
-    },
-    {
-        title: 'How to manage time for the ACT Math test',
-        id: '165519952'
-    },
-    {
-        title: 'Should I skip any questions on the ACT reading test?',
-        id: '170987104'
-    },
-    {
-        title: 'How should I bubble for the ACT reading test?',
-        id: '165700827'
-    },
-    {
-        title: 'Conflicting viewpoints passage strategy',
-        id: '163876840',
-        restricted: true
-    },
-    {
-        title: 'Non-conflicting viewpoints passages strategy',
-        id: '163876836',
-        restricted: true
-    },
-    {
-        title: 'Using your finger to save time',
-        id: '163876833'
-    },
+  {
+    title: "PDF - Strategy summary",
+    id: "science-strategy",
+    pdf: "/review-guides/science-strategy.pdf",
+    restricted: true,
+    buttonText: "Open PDF",
+    isPdf: true,
+  },
+  {
+    title: "Know the ACT reading test BEFORE you take it",
+    id: "163876887",
+  },
+  {
+    title: "How to manage time for the ACT Math test",
+    id: "165519956",
+  },
+  {
+    title: "Should I skip any questions on the ACT reading test?",
+    id: "163876842",
+  },
+  {
+    title: "How should I bubble for the ACT reading test?",
+    id: "165700826",
+  },
+  {
+    title: "Conflicting viewpoints passage strategy",
+    id: "163876840",
+    restricted: true,
+  },
+  {
+    title: "Non-conflicting viewpoints passages strategy",
+    id: "163876836",
+    restricted: true,
+  },
+  {
+    title: "Using your finger to save time",
+    id: "163876833",
+  },
 ];
 
 export {
-    VIDEOS_GENERAL,
-    VIDEOS_ENGLISH_STRATEGIES,
-    VIDEOS_MATH_STRATEGIES,
-    VIDEOS_READING_STRATEGIES,
-    VIDEOS_SCIENCE_STRATEGIES
+  VIDEOS_GENERAL,
+  VIDEOS_ENGLISH_STRATEGIES,
+  VIDEOS_MATH_STRATEGIES,
+  VIDEOS_READING_STRATEGIES,
+  VIDEOS_SCIENCE_STRATEGIES,
 };
